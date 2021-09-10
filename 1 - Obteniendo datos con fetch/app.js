85509f8e03306ddda727db378b184e903352605f
ferney <williamchaparro@unisangil.edu.co>
2021-09-10 10:55:10 -0500

    first commit

---
+++ 1 - Obteniendo datos con fetch/app.js
@@ -0,0 +1,35 @@
+"use strict";
+
+//********************************
+//*** Obteniendo datos con fetch
+
+var boton = document.getElementById('boton');
+var contenedor = document.getElementById('contenedor');
+var posts = null;
+
+boton.addEventListener('click', function () {
+
+    fetch('http://jsonplaceholder.typicode.com/posts')
+    .then(data => data.json())
+    .then(data => {
+        posts = data;
+        mostrarDatos(posts)
+    })
+
+});
+
+function mostrarDatos(posts) {
+    posts.map((post, i) => {
+        let titulo = document.createElement('h1');
+        let contenido = document.createElement('p');
+let email = document.createElement('p');
+
+        titulo.innerHTML = (i + 1) + " - " + post.title;
+        contenido.innerHTML = post.body;
+email.innerHTML = post.email;
+
+        contenedor.appendChild(titulo);
+        contenedor.appendChild(contenido);
+	contenedor.appendChild(email);
+    })
+}
