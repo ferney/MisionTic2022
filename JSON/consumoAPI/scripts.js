98519626b07ba315f0343fea7abe9d0a3ca88537
ferney <williamchaparro@unisangil.edu.co>
2021-09-30 08:02:15 -0500

    Update scripts.js

--- JSON/consumoAPI/scripts.js
+++ JSON/consumoAPI/scripts.js
@@ -1,4 +1,5 @@
-const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+//COMENTO EL API KEY PARA QUE NO GENERE ALERTAS
+//const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_bypopularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
